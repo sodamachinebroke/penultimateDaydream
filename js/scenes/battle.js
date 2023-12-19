abb2511e2853708471617f9e6a36f9f20601fb0f
sodamachinebroke <79090166+sodamachinebroke@users.noreply.github.com>
2023-12-19 18:02:46 +0100

    implemented the attack function and the heal function

--- js/scenes/battle.js
+++ js/scenes/battle.js
@@ -4,6 +4,8 @@ class BattleScene extends Phaser.Scene {
     constructor(title) {
         super({ key: title });
     }
+    //We need this to determine whether our guy can attack
+    lockUi = false;
 
     preload() {
 
@@ -16,7 +18,7 @@ class BattleScene extends Phaser.Scene {
         const mainScreen = this.scene.get('game');
         const player = mainScreen.player;
 
-        
+
         this.playerHealth = 50;
 
 
@@ -26,6 +28,19 @@ class BattleScene extends Phaser.Scene {
 
         this.battleMenu();
         this.createEnemyGroup();
+
+        this.noEnemyText = this.add.text(
+            this.cameras.main.width / 2,
+            this.cameras.main.height / 2,
+            'No valid enemy selected!',
+            {
+                fill: '#fc0000',
+                fontFamily: 'Arial',
+                fontSize: '48px',
+            }
+        );
+        this.noEnemyText.setOrigin(0.5);
+        this.noEnemyText.setVisible(false);
     }
     createEnemyGroup() {
         this.enemyGroup = this.physics.add.group({
@@ -39,6 +54,7 @@ class BattleScene extends Phaser.Scene {
             createCallback: function (enemy) {
                 enemy.setScale(0.4, 0.4);
                 enemy.health = 10;
+                enemy.defeated = false;
             }
         });
         let enemyCount = 0;
@@ -69,9 +85,9 @@ class BattleScene extends Phaser.Scene {
 
     handleEnemyButtonClick(enemyIndex) {
 
-
+        /* I'm gonna be honest, i do not remember why we need to mess around in the registry, but this seemed logical. Hopefully it's safe too.*/
         this.registry.set('enemyIndex', enemyIndex);
-        this.battleMenuContainer.list.slice(4, 7).forEach((enemyButton, index) => {
+        this.battleMenuContainer.list.slice(5, 8).forEach((enemyButton, index) => {
 
             if (index != this.registry.get('enemyIndex'))
                 this.tweens.add({
@@ -83,8 +99,8 @@ class BattleScene extends Phaser.Scene {
         });
 
 
-        // Get the selected enemy button and apply a tween to highlight it
-        const selectedEnemyButton = this.battleMenuContainer.getAt(enemyIndex + 4);
+        // Highlight the selected enemy button
+        const selectedEnemyButton = this.battleMenuContainer.getAt(enemyIndex + 5);
         if (selectedEnemyButton) {
             this.selectEnemy = this.tweens.add({
                 targets: selectedEnemyButton,
@@ -93,14 +109,21 @@ class BattleScene extends Phaser.Scene {
                 duration: 500,
                 ease: 'Linear',
             });
+
+            this.selectedEnemy = this.enemyGroup.getChildren()[enemyIndex];
         }
     }
     battleFunction() {
 
-        for (this.turn = 0; this.turn < 4; this.turn++) {
-            switch (this.turn % 4) {
+        this.lockUi = true;
+        const maxTurns = 4;
+        let currentTurn = 0;
+
+        const executeTurn = () => {
+            const selectedEnemyIndex = this.registry.get('enemyIndex');
+
+            switch (currentTurn % 4) {
                 case 0:
-                    const selectedEnemyIndex = this.registry.get('enemyIndex');
                     if (selectedEnemyIndex !== -1) {
                         this.playerAttack(selectedEnemyIndex);
                     }
@@ -114,39 +137,73 @@ class BattleScene extends Phaser.Scene {
                 case 3:
                     this.enemyAttack(2);
                     break;
-
                 default:
                     break;
             }
 
-            console.log("Turn mod 4: " + this.turn);
-        }
+            console.log("Turn mod 4: " + currentTurn);
+            currentTurn++;
+
+            if (currentTurn < maxTurns) {
+                // Execute the next turn after a delay
+                this.time.delayedCall(2000, executeTurn, [], this);
+            } else {
+                //Unlock UI
+                this.lockUi = false;
+
+            }
+        };
+
+        // Start the turn sequence
+        executeTurn();
     }
 
 
-    
+
+
+
+
     playerAttack(enemyIndex) {
         console.log('Player attacks enemy: ' + (enemyIndex + 1));
 
-        const selectedEnemy = this.enemyGroup.getChildren()[enemyIndex];
+        const selectedEnemy = this.selectedEnemy;
 
-        if (selectedEnemy) {
+        if (selectedEnemy && !selectedEnemy.defeated) {
             const damage = Phaser.Math.Between(1, 10);
             selectedEnemy.health -= damage;
             console.log(`Player dealt: ${damage} to enemy ${enemyIndex + 1} - HP: ${selectedEnemy.health}`);
-            const enemyButton = this.battleMenuContainer.getAt(enemyIndex+4);
-            if(enemyButton){
-                enemyButton.setText(`Enemy ${enemyIndex+1} - HP: ${selectedEnemy.health}`);
+            const enemyButton = this.battleMenuContainer.getAt(enemyIndex + 5);
+            if (enemyButton) {
+                enemyButton.setText(`Enemy ${enemyIndex + 1} - HP: ${selectedEnemy.health}`);
+                if (selectedEnemy.health <= 0) {
+                    // Enemy defeated, update defeated flag
+                    selectedEnemy.defeated = true;
+                    this.defeatedEnemies++;
+                    console.log(`Enemy ${enemyIndex + 1} defeated!`);
+
+                    if (this.defeatedEnemies === this.enemyGroup.getChildren().length) {
+                        // All enemies defeated, transition to winScene
+                        console.log('All enemies defeated! Transitioning to winScene');
+                        this.scene.start('winScene');
+                    }
+                }
             }
         }
     }
 
-    enemyAttack(enemyIndex){
+
+
+
+    enemyAttack(enemyIndex) {
         //console.log('Enemy ' + (enemyIndex + 1) + ' attacks');
-        const damage = Phaser.Math.Between(1,10);
+        const damage = Phaser.Math.Between(1, 10);
         console.log(`Initial player health: ${this.playerHealth}`);
         this.playerHealth -= damage;
         console.log(`Enemy dealt: ${damage} to the player- Player HP: ${this.playerHealth}`);
+        const playerHealthText = this.battleMenuContainer.getAt(4);
+        if (playerHealthText) {
+            playerHealthText.setText(`Player Health: ${this.playerHealth}`);
+        }
     }
 
 
@@ -158,11 +215,6 @@ class BattleScene extends Phaser.Scene {
 
         this.battleMenuContainer.add(bMenuBG);
 
-        this.playerHealthText = this.add.text(20, 20, `Player Health: ${this.playerHealth}`,{
-            fill: '#ffffff',
-            fontFamily:'Arial',
-            fontSize: '20px',
-        });
 
         const buttonTexts = ['Attack', 'Heal', 'Flee'];
         buttonTexts.forEach((text, index) => {
@@ -183,19 +235,72 @@ class BattleScene extends Phaser.Scene {
             });
             this.battleMenuContainer.add(button);
         });
+        const playerHealthText = this.add.text(300, 50, `Player Health: ${this.playerHealth}`, {
+            fill: '#ffffff',
+            fontFamily: 'Arial',
+            fontSize: '20px',
+        });
 
-        
+        playerHealthText.setOrigin(0, 0);
+        this.battleMenuContainer.add(playerHealthText);
 
 
 
     }
 
+    displaySplashText(text) {
+        this.clearMessages();
+
+        const centerX = this.cameras.main.width / 2;
+        const centerY = this.cameras.main.height / 2;
+
+        this.noEnemyText.setText(text);
+        this.noEnemyText.setPosition(centerX, centerY);
+        this.noEnemyText.setVisible(true);
+
+        setTimeout(() => {
+            this.clearMessages();
+        }, 2000);
+    }
+
+    clearMessages() {
+        this.noEnemyText.setVisible(false);
+    }
+    playerHeal() {
+
+        this.playerHealth += 10;
+        const playerHealthText = this.battleMenuContainer.getAt(4);
+        if (playerHealthText) {
+            playerHealthText.setText(`Player Health: ${this.playerHealth}`);
+        }
+
+    }
+
     handleButtonClick(buttonText) {
+        if (this.lockUi) {
+            this.displaySplashText('Currently cannot interact!');
+            return;
+        }
+        const selectedEnemyIndex = this.registry.get('enemyIndex');
+
+        console.log("Selected enemy index: " + selectedEnemyIndex);
+        console.log("Selected enemy: " + this.selectedEnemy);
         switch (buttonText) {
             case 'Attack':
-                this.battleFunction();
+                if (selectedEnemyIndex > -1 && selectedEnemyIndex < 3 && this.selectedEnemy && !this.selectedEnemy.defeated) {
+                    this.battleFunction();
+                } else {
+                    this.displaySplashText('No enemy selected!');
+                }
+
                 break;
             case 'Heal':
+                if (this.playerHealth <= 40) {
+                    this.playerHeal();
+                } else {
+                    this.displaySplashText('Cannot overheal!');
+                }
+
                 break;
             case 'Flee':
                 this.bMusic.stop();
